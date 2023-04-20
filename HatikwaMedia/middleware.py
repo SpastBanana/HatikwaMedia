ea8c4378ae292e5d07d416f91e5f6d7d413582ba
Sebastiaan de Vries <sebastiaan.j.d.v@outlook.com>
2023-04-20 10:14:41 +0200

    Backup - try of fixing Middleware

--- HatikwaMedia/middleware.py
+++ HatikwaMedia/middleware.py
@@ -2,20 +2,31 @@ from django.conf import settings
 from django.http import Http500
 from django.shortcuts import redirect
 
-class admin_required:
+# class yo:
 
+#     def __init__(self, get_response):
+#         self.get_response = get_response
+
+#     def __call__(slef, request):
+#         response = self.get_response(request)
+#         return response
+
+#     def process_view(self, request, view_func, view_args, view_kwargs):
+#         assert hasattr(request, 'user')
+
+#         if not request.user.is_authenticated():
+#             return redirect('/login')
+
+#         if not request.user.groups.filter(name="Admin").exists():
+#             raise Http500
+
+
+class dbadmin:
     def __init__(self, get_response):
         self.get_response = get_response
 
-    def __call__(slef, request):
+    def __call__(self, request):
+
         response = self.get_response(request)
+
         return response
-
-    def process_view(self, request, view_func, view_args, view_kwargs):
-        assert hasattr(request, 'user')
-
-        if not request.user.is_authenticated():
-            return redirect('/login')
-
-        if not request.user.groups.filter(name="Admin").exists():
-            raise Http500
--- HatikwaMedia/settings.py
+++ HatikwaMedia/settings.py
@@ -43,7 +43,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'HatikwaMedia.middleware.admin_required',
+    'HatikwaMedia.middleware.dbadmin'
 ]
 
 ROOT_URLCONF = 'HatikwaMedia.urls'
